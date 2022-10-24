da6a2b2ca0b7aebf998067bf637cce12c412cedb
YamilUchani <yamilronalduchaniguachalla@gmail.com>
2022-10-24 17:15:33 -0400

    Yamil Uchani
    Configuracion de comunicacion de envio de imagenes para el software de Unity

--- Robot_acuatico_autonomo/Assets/Scripts/Movimiento.cs
+++ Robot_acuatico_autonomo/Assets/Scripts/Movimiento.cs
@@ -1,25 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Net;
+//Esta clase del namespace Net se encarga de el envio y recibo de datos
+using System.Net.Sockets;
+//Esta clase se encarga de la creacion de subprocesos, los cuales permitiran el envio y recibo de datos en mas de un subproceso.
+using System.Threading;
+using System.Text;
 public class Movimiento : MonoBehaviour
 {
+    Thread mThread;
+    public string connectionIP= "127.0.0.3";
+    public int connectionPort=2100;
+    IPAddress localadd;
+    TcpClient cleint;
+    TcpListener listener;
     public GameObject cam;
     public float aceleracion=500f;
     public float aceleracionangular=5f;
     public float aceleracionactual=0f;
     public float aceleracionangularactual=0f;
+    private float tiemporespuesta;
     public float giro=0f;
     public Rigidbody rb;
+    public int movHorizontal=0;
+    public int movVertical=0;
+    private bool running;
     private void Start()
     {
         rb=GetComponent<Rigidbody>();   
+        ThreadStart ts = new ThreadStart(GetInfo);
+        mThread=new Thread(ts);
+        mThread.Start(); 
+    }
+    void GetInfo()
+    {
+        localadd= IPAddress.Parse(connectionIP);
+        listener= new TcpListener(IPAddress.Any, connectionPort);
+        listener.Start();
+
+        cleint=listener.AcceptTcpClient();
+        running=true;
+        while(running)
+        {
+            recibodedatos();
+        }
+        listener.Stop();
+    }
+    private void recibodedatos()
+    {
+        NetworkStream nwStream=cleint.GetStream();
+        byte[] buffer=new byte[cleint.ReceiveBufferSize];
+        int bytesRead=nwStream.Read(buffer,0,cleint.ReceiveBufferSize);
+        string Dato=Encoding.UTF8.GetString(buffer,0,bytesRead);
+        if(Dato!=null)
+        {
+            switch (Dato)
+            {
+                case "derecha":
+                    movHorizontal=1;
+                    break;
+                case "izquierda":
+                     movHorizontal=-1;
+                    break;
+                case "arriba":
+                    movVertical=1;
+                    break;
+                case "abajo":
+                     movVertical=-1;
+                    break;    
+            }
+        }
     }
     private void FixedUpdate()
     {
-        aceleracionactual=aceleracion*Input.GetAxis("Vertical");
-        aceleracionangularactual=aceleracionangular*Input.GetAxis("Horizontal");
+        
+        aceleracionactual=aceleracion*movVertical;
+        aceleracionangularactual=aceleracionangular*movHorizontal;
         rb.AddForce((transform.position-cam.transform.position)*aceleracionactual);
         rb.AddTorque(Vector3.up*aceleracionangularactual*Time.deltaTime);
-    }
+        movHorizontal=0;
+        movVertical=0;
+    }   
+        
 }
--- Robot_acuatico_autonomo/Assets/Scripts/flotacion.cs
+++ Robot_acuatico_autonomo/Assets/Scripts/flotacion.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class flotacion : MonoBehaviour
 {
+    /*Este script esta activo en el gameObject de "bote", dentro de cada elemento
+    hijo del mismo, que actuan para flotar en ciertos puntos del bote.
+    Este script se encarga de regular el nivel que se sumerge el objeto
+    */
+    //Esta variable se encarga de consultar el rigidbody del bote
     public GameObject Objeto;
+    //Esta variable se encarga de definir el gameobject que actuara como el agua
     public GameObject agua;
-    public float nivelagua=0.0f;
-    public float umbralagua=2.0f;
-    public float densidadagua=0.125f;
-    public float fuerzaabajo=4.0f;
-
+    //Esta variable se encarga de definir el nivel de flotacion del agua, el cual
+    //será el mismo nivel que el gamObject del agua
+    public float nivelagua;
+    //Esta variable se encarga de regular el crecimiento de la fuerza de empuje.
+    public float umbralagua;
+    //Esta variable se encarga de definir la velocidad  del cambio de direccion de la fuerza de empuje
+    //Si la misma es muy alta, actua como una cama elastica.
+    public float densidadagua;
+    //Esta variable se encarga de agregar una fuerza extra a la fuerza de la gravedad
+    public float fuerzaabajo;
+    //Esta variable se trata de la fuerza de empuje
     float fuerzafactor;
-    Vector3 fuerzafloat;
+    //Esta variable se encarga de la fuerza total que se aplica al objeto
+    public Vector3 fuerzafloat;
     private void Start()
     {
         nivelagua=agua.transform.position.y;
@@ -20,7 +33,7 @@ public class flotacion : MonoBehaviour
     private void FixedUpdate()
     {
         fuerzafactor=1.0f-((transform.position.y-nivelagua)/umbralagua);
-        if(fuerzafactor>0.0f)
+        if(fuerzafactor>1.0f)
         {
             fuerzafloat=-Physics.gravity*(fuerzafactor-Objeto.GetComponent<Rigidbody>().velocity.y*densidadagua);
             fuerzafloat+=new Vector3(0.0f,-fuerzaabajo,0.0f);
