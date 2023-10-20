ec48a178c7da71b3d4365d1ff7fa195c0a143eca
YamilUchani <46763987+YamilUchani@users.noreply.github.com>
2023-10-20 14:13:44 -0400

    Yamil Uchani laptop

--- Robot_acuatico_autonomo/Assets/Shader/deph.cs
+++ Robot_acuatico_autonomo/Assets/Shader/deph.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System;
 using System.Net.Sockets;
+using System.Collections.Generic;
 
 public class deph : MonoBehaviour
 {
@@ -8,58 +9,78 @@ public class deph : MonoBehaviour
     private Texture2D[] textures = new Texture2D[3];
     public Camera[] cameras = new Camera[3]; // Asigna tus cámaras en el inspector
     private float lastSendTime; // Para rastrear el tiempo de envío
+    private int currentIndex = 0;
+    private Queue<byte[]> imageQueue = new Queue<byte[]>();
     private int i;
+
     private void OnEnable()
     {
-        // Inicializa los RenderTextures
-        for (int i = 0; i < 3; i++)
-        {
-            renderTextures[i] = new RenderTexture(600, 250, 24);
-            renderTextures[i].Create();
-            cameras[i].targetTexture = renderTextures[i];
-            textures[i] = new Texture2D(renderTextures[i].width, renderTextures[i].height);
-        }
-
         lastSendTime = Time.time; // Inicializa el tiempo de envío
     }
 
     private void Update()
     {
         // Comprueba si ha pasado al menos 10 segundos desde el último envío
-        if (Time.time - lastSendTime >= 0.7)
+        if (Time.time - lastSendTime >= 1)
         {
-            RenderTexture.active = renderTextures[i];
-            textures[i].ReadPixels(new Rect(0, 0, renderTextures[i].width, renderTextures[i].height), 0, 0);
-            textures[i].Apply();
-            byte[] data0 = textures[i].EncodeToPNG();
-            SendToServer(data0);
-            i++;
+            RenderTexture rt = new RenderTexture(960,540, 24);
+            Texture2D screenShot = new Texture2D(960, 540, TextureFormat.RGB24, false);
+            cameras[i].targetTexture = rt;
+            cameras[i].Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, 960, 540), 0, 0);
+            cameras[i].targetTexture = null;
+            RenderTexture.active = null;
+            Destroy(rt);
+            byte[] data = screenShot.EncodeToPNG();
+
+            // Encolar la imagen para el envío
+            imageQueue.Enqueue(data);
+
+            // Pasar al siguiente índice
+            currentIndex = (currentIndex + 1) % 3;
+
             lastSendTime = Time.time;
+            i++;
+            if(i>=2+1)
+            {
+                i=0;
+            }
         }
-        if(i>2)
+
+        // Comprueba si hay imágenes en la cola y envía una a la vez
+        if (imageQueue.Count > 0)
         {
-            i=0;
+            byte[] imageData = imageQueue.Dequeue();
+            SendToServer(imageData);
         }
+        
     }
 
-    private void SendToServer(byte[] data0)
+    private void SendToServer(byte[] data)
+{
+    try
     {
-        try
-        {
-            // Establece una conexión con el servidor Python
-            TcpClient client = new TcpClient("127.0.0.1", 12345); // Ajusta la dirección IP y el puerto
+        // Establece una conexión con el servidor Python
+        TcpClient client = new TcpClient("127.0.0.1", 12345); // Ajusta la dirección IP y el puerto
 
-            NetworkStream stream = client.GetStream();
+        NetworkStream stream = client.GetStream();
 
-            // Envía los datos
-            stream.Write(data0, 0, data0.Length);
+        // Agrega la marca "END_OF_IMAGE" al final de los datos
+        byte[] dataWithMarker = new byte[data.Length + 12]; // 12 bytes para "END_OF_IMAGE"
+        data.CopyTo(dataWithMarker, 0);
+        byte[] marker = System.Text.Encoding.ASCII.GetBytes("END_OF_IMAGE");
+        marker.CopyTo(dataWithMarker, data.Length);
 
-            // Cierra la conexión
-            client.Close();
-        }
-        catch (Exception e)
-        {
-            Debug.LogError("Error al enviar datos: " + e.Message);
-        }
+        // Envía los datos
+        stream.Write(dataWithMarker, 0, dataWithMarker.Length);
+
+        // Cierra la conexión
+        client.Close();
     }
+    catch (Exception e)
+    {
+        Debug.LogError("Error al enviar datos: " + e.Message);
+    }
+}
 }
--- Robot_acuatico_autonomo/server/server_deph.py
+++ Robot_acuatico_autonomo/server/server_deph.py
@@ -17,26 +17,64 @@ img_counter = 0
 
 while True:
     connection, address = server.accept()
-    data = connection.recv(7000000)  # Lee los datos de la imagen (ajusta el tamaño según tus necesidades)
+    data = b''  # Inicializa un búfer de datos vacío
 
-    # Convierte los datos en una imagen
-    nparr = np.frombuffer(data, np.uint8)
-    img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)  # Ahora se espera una imagen a color
+    # Lee los datos de la imagen en fragmentos y busca un marcador para delimitar cada imagen
+    while True:
+        chunk = connection.recv(1024)
+        if not chunk:
+            break
+        data += chunk
 
-    # Comprueba si la imagen es en color o en blanco y negro
-    if img_np is not None:
-        img_counter += 1
-        if img_counter % 3 == 0:
-            # La tercera imagen es a color
-            filename = f'color.png'
-            cv2.imwrite(filename, img_np)
-        else:
-            # Las dos primeras imágenes son en blanco y negro
-            img_np_bw = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
-            filename = f'bicolor{bicolor_count % 2 + 1}.png'
-            cv2.imwrite(filename, img_np_bw)
-            bicolor_count += 1
-    else:
-        print("Failed to decode image data")
+        # Busca un marcador para determinar el final de una imagen
+        if b'END_OF_IMAGE' in data:
+            img_data = data.split(b'END_OF_IMAGE')
+            for img_chunk in img_data:
+                if len(img_chunk) == 0:
+                    continue
 
-    connection.close()
+                # Convierte los datos en una imagen
+                nparr = np.frombuffer(img_chunk, np.uint8)
+                img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+                # Comprueba si la imagen es en color o en blanco y negro
+                if img_np is not None:
+                    img_counter += 1
+                    if img_counter % 3 == 0:
+                        filename = f'color{1}.png'
+
+                    else:
+                        img_np = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+                        filename = f'bicolor{bicolor_count % 2 + 1}.png'
+                        bicolor_count += 1
+
+                    cv2.imwrite(filename, img_np)
+
+            data = b''  # Reinicia el búfer de datos
+    img1 = cv2.imread('bicolor1.png', cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread('bicolor2.png', cv2.IMREAD_GRAYSCALE)
+
+    # Asegurarse de que las imágenes tienen el mismo tamaño
+    if img1.shape != img2.shape:
+        print("Las imágenes deben tener el mismo tamaño")
+        exit()
+
+    # Calcular la matriz de distancia
+    distancia = np.abs(img1.astype(int) - img2.astype(int))
+
+    # Normalizar la matriz de distancia para que los valores estén entre 0 y 1
+    distancia_normalizada = cv2.normalize(distancia, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+    # Crear una imagen en color para visualizar la profundidad
+    profundidad_color = np.zeros((img1.shape[0], img1.shape[1], 3), dtype=np.uint8)
+
+    # Mapear los valores de profundidad a colores en el espectro del rojo al violeta
+    profundidad_color[:, :, 0] = 255 * distancia_normalizada  # canal azul
+    profundidad_color[:, :, 1] = 255 * (1 - distancia_normalizada)  # canal verde
+    profundidad_color[:, :, 2] = 255 * distancia_normalizada  # canal rojo
+    filename = f'profcolor{1}.png'
+    cv2.imwrite(filename, profundidad_color)
+
+
+
+    
